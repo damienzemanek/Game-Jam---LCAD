58749540e4175244907edb69f503839787d82c1f
Emil Z <damienzemanek@lcad.edu>
2025-11-08 16:31:32 -0800

    slider ui updates

--- Assets/Game Files/Scripts/Player/Health.cs
+++ Assets/Game Files/Scripts/Player/Health.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Extensions;
 using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.UI;
 
 public abstract class Health : MonoBehaviour
 {
+    [SerializeField] Slider slider;
     [SerializeField] protected float maxHp;
     [SerializeField] protected float currentHp;
     [SerializeField] protected UnityEventPlus onDeathHook;
@@ -13,6 +16,13 @@ public abstract class Health : MonoBehaviour
     {
         if (onDeathHook == null) onDeathHook = new UnityEventPlus();
         maxHp = currentHp;
+        UpdateSliderUI();
+
+    }
+
+    private void OnEnable()
+    {
+        UpdateSliderUI();
     }
 
 
@@ -20,11 +30,20 @@ public abstract class Health : MonoBehaviour
     public void TakeDamage(float amount)
     {
         currentHp -= amount;
+        UpdateSliderUI();
+
         if (currentHp < 0)
         {
             onDeathHook?.get?.Invoke();
             Die();
         }
+    }
+
+    public void UpdateSliderUI()
+    {
+        this.Log("Updating slider value");
+        float sliderVal = currentHp / maxHp;
+        slider.value = sliderVal;
     }
 
     public abstract void Die();
